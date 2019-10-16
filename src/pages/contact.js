4f9998af995c73e16c41e191bb53f1c462c9a8a2
Rachel Gould <rachel@rachelgould.dev>
2019-10-16 16:51:28 -0700

    refactoring

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Helmet from "react-helmet";
 import { graphql } from "gatsby";
 import styled from "@emotion/styled";
@@ -8,7 +8,10 @@ const ContactTitle = styled("h1")`
     margin-bottom: 1em;
 `
 
-const embedCode = `<div class="typeform-widget" data-url="https://rachelgould219290.typeform.com/to/MC4n7J" style="width: 100%; height: 500px;"></div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script> <div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;"> powered by <a href="https://admin.typeform.com/signup?utm_campaign=MC4n7J&utm_source=typeform.com-14848761-Free&utm_medium=typeform&utm_content=typeform-embedded-poweredbytypeform&utm_term=EN" style="color: #999" target="_blank">Typeform</a> </div>`
+const typeFormScript = () => {
+  var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; 
+  if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) }
+}
 
 const Contact = ({ title, meta }) => (
     <>
@@ -54,7 +57,8 @@ const Contact = ({ title, meta }) => (
             <ContactTitle>
                 {title}
             </ContactTitle>
-            <div className="typeform-widget" data-url="https://rachelgould219290.typeform.com/to/MC4n7J" id="typef_orm" style={{width: 100 + '%', height: 500 + 'px'}}></div> {(function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()}
+            <div className="typeform-widget" data-url="https://rachelgould219290.typeform.com/to/MC4n7J" id="typef_orm" style={{width: 100 + '%', height: 500 + 'px'}}></div>
+            {typeFormScript()}
         </Layout>
     </>
 );
